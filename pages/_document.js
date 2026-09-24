dca448169db2541dc48391b229e0f867d97ee638
hooksscreener <jhooks.business@gmail.com>
2026-09-24 00:09:06 -0400

    Add files via upload

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="Hooks Workout" />
+        <meta name="theme-color" content="#000000" />
+        <title>Hooks Workout</title>
       </Head>
       <body>
         <Main />
